ef28103e3ffbe59f422e5843531194f0fbeac948
Svitlana R <sv73@ukr.net>
2022-08-12 19:48:02 +0300

    Added link to Pexel on Footer

--- src/Footer.js
+++ src/Footer.js
@@ -17,8 +17,9 @@ export default function Footer() {
         | Open-sourced code is on&nbsp;
         <a href="https://github.com/Switlo/dictionary-react-project" target="_blank" rel="noopener noreferrer">
           GitHub
-        </a>
-        &nbsp;and hosted on <a href="https://keen-jelly-5530d0.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a>
+        </a>&nbsp;
+        and hosted on <a href="https://keen-jelly-5530d0.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a>&nbsp;
+        | Photos provided by&nbsp;<a href="https://www.pexels.com" target="_blank" rel="noopener noreferrer">Pexels</a>
       </p>
     </div>
   );
